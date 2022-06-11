470e57bc10bdb273dd26af2db1f2240d9dbed9b4
Sutolix <tg884ght@gmail.com>
2022-06-11 18:09:27 -0300

    restore bot configs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config({path:__dirname+'/../.env'})
 
 const express = require('express');
-// const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 
 const app = express();
 app.use(express.json());
@@ -9,30 +9,29 @@ app.use(express.json());
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.BOT_API;
 
-console.log(token)
-// // Create a bot that uses 'polling' to fetch new updates
-// const bot = new TelegramBot(token, {polling: true});
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
 
-// // Matches "/echo [whatever]"
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//   // 'msg' is the received Message from Telegram
-//   // 'match' is the result of executing the regexp above on the text content
-//   // of the message
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-//   const chatId = msg.chat.id;
-//   const resp = match[1]; // the captured "whatever"
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
 
-//   // send back the matched "whatever" to the chat
-//   bot.sendMessage(chatId, resp);
-// });
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
 
-// // Listen for any kind of message. There are different kinds of
-// // messages.
-// bot.on('message', (msg) => {
-//   const chatId = msg.chat.id;
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
 
-//   // send a message to the chat acknowledging receipt of their message
-//   bot.sendMessage(chatId, 'Received your message');
-// });
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
 
 app.listen(process.env.PORT || 3333);
